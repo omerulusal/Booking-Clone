bd51eb047cd09246633f8317e0f4e8058c957ebe
Ömer Ulusal <96357374+omerulusal@users.noreply.github.com>
2023-12-17 15:08:45 +0300

    componentsUpdate

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./_header.scss"
-import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns';
+import Options from "../options/Options";
 const Header = () => {
+
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ]);
+    const [display, setDisplay] = useState(false);
     return (
         <div className="header">
             <div className="headerContainer">
@@ -21,6 +36,35 @@ const Header = () => {
                     <div className="headerListItem">
                         <FontAwesomeIcon icon={faTaxi} />
                         <span>Airport taxis</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+                <p className="headerDesc">Get rewarded for your travels – unlock instant savings of 10% or more with a free OM3RBooking account</p>
+                <button className="headerBtn">Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                        <input type="text" placeholder="Where are you going" className="headerSearchInput" />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                        <span className="headerSearchText" onClick={() => setDisplay(!display)} > {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`} </span>
+                        {/* span içinde tarihleri yazdırdık */}
+                        {display && <DateRangePicker
+                            onChange={item => setDate([item.selection])}
+                            showSelectionPreview={true}
+                            moveRangeOnFirstSelection={false}
+                            months={1}
+                            ranges={date}
+                            direction="horizontal"
+                            className="date"
+                        />}
+                    </div>
+                    <div className="headerSearchItem">
+                        <Options />
+                    </div>
+                    <div className="headerSearchItem">
+                        <button className="headerBtn">Search</button>
                     </div>
                 </div>
             </div>
---
+++ client/src/components/options/Options.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./_options.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPerson } from "@fortawesome/free-solid-svg-icons";
+
+const Options = () => {
+    const [openOptions, setOpenOptions] = useState(false);
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 0
+    })
+    return (
+        <>
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span onClick={() => setOpenOptions(!openOptions)} className="headerSearchText">{`${options.adult} adult · ${options.children} children · ${options.room}`}</span>
+            {openOptions && <div className="options">
+                <div className="optionItem">
+                    <span className="optionText">Adult</span>
+                    <button className="optionCounterBtn" onClick={() => options.adult > 1 && setOptions({ ...options, adult: options.adult - 1 })}>-</button>
+                    {/* en az 1 yetişkin olmak zorunda. setOptions ile tum state degerlerini aldım ( ardından yetişkin sayısını 1 azalttım */}
+                    <span className="optionCounterNumber">{options.adult}</span>
+                    <button className="optionCounterBtn" onClick={() => options.adult < 5 && setOptions({ ...options, adult: options.adult + 1 })}>+</button>
+                </div>
+                <div className="optionItem">
+                    <span className="optionText">Children</span>
+                    <button className="optionCounterBtn" onClick={() => options.children > 0 && setOptions({ ...options, children: options.children - 1 })}>-</button>
+                    <span className="optionCounterNumber">{options.children}</span>
+                    <button className="optionCounterBtn" onClick={() => options.children < 4 && setOptions({ ...options, children: options.children + 1 })}>+</button>
+                </div>
+                <div className="optionItem">
+                    <span className="optionText">Room</span>
+                    <button className="optionCounterBtn" onClick={() => options.room > 1 && setOptions({ ...options, room: options.room - 1 })}>-</button>
+                    <span className="optionCounterNumber">{options.room}</span>
+                    <button className="optionCounterBtn" onClick={() => options.room < 3 && setOptions({ ...options, room: options.room + 1 })}>+</button>
+                </div>
+            </div>}
+        </>
+    )
+}
+
+export default Options
